7fca47a9293c6bda3b38f8f40a4615b2eb2246bd
Sam Eaton <same@qualtrics.com>
2016-10-17 09:00:51 -0600

    finishes first version of install script

--- install.rb
+++ install.rb
@@ -1,6 +1,34 @@
 require "net/http"
 require "uri"
+require "yaml"
+require 'fileutils'
 
-response = Net::HTTP.get_response("https://raw.githubusercontent.com/samueleaton/sentry/master/install.rb")
-puts "response: #{response}"
-puts "response.body: #{response.body}"
+uri = URI.parse("https://raw.githubusercontent.com/samueleaton/sentry/master/src/sentry.cr")
+response = Net::HTTP.get_response(uri)
+
+if response.code.to_i > 299
+  puts "HTTP request error"
+  puts response.msg
+  exit 1
+end
+
+sentry_code = response.body
+
+begin
+  shard_yml = YAML.load(File.read "./shard.yml")
+  raise "missing key in shard.yml: name" unless shard_yml.has_key? "name"
+rescue => e
+  puts "Error with shard.yml"
+  puts e
+  exit 1
+end
+
+app_name = shard_yml["name"]
+sentry_code.gsub!(/\[app_name\]/, app_name)
+
+FileUtils.mkdir_p "./dev"
+File.write "./dev/sentry.cr", sentry_code
+
+puts "🤖 sentry is vigilant. beep-boop..."
+puts "\nTo run, do:
+  crystal ./dev/sentry.cr\n\n"
