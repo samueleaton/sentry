3ba55350f60cffcfb0adb4cebd9a7fca739314ca
Sam Eaton <same@qualtrics.com>
2016-10-17 08:31:44 -0600

    implements correct path for install script

--- install.rb
+++ install.rb
@@ -1,6 +1,6 @@
 require "net/http"
 require "uri"
 
-response = Net::HTTP.get_response("https://raw.githubusercontent.com/samueleaton/sentry/v0.1.0/install.rb")
+response = Net::HTTP.get_response("https://raw.githubusercontent.com/samueleaton/sentry/master/install.rb")
 puts "response: #{response}"
 puts "response.body: #{response.body}"
