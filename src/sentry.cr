fdc9a9052bafeb6e0543d2a15347ecaf8e76ef1b
Sam Eaton <same@qualtrics.com>
2016-10-17 13:53:18 -0600

    removes shell for app process

--- src/sentry.cr
+++ src/sentry.cr
@@ -9,11 +9,11 @@ module Sentry
             build_process : (Nil | Process::Status) = nil
     
     private def build_app_process
-      @build_process = Process.run(BUILD_APP_COMMAND, output: true, error: true)
+      @build_process = Process.run(BUILD_APP_COMMAND, shell: true, output: true, error: true)
     end
 
     private def create_app_process
-      @app_process = Process.new(RUN_APP_COMMAND, shell: true, output: true, error: true)
+      @app_process = Process.new(RUN_APP_COMMAND, output: true, error: true)
     end
 
     private def get_timestamp(file : String)
