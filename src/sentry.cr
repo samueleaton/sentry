610d52076888ddd35a62ebb1342bc5798e8945d0
Sam Eaton <same@qualtrics.com>
2016-12-14 08:45:59 -0800

    Adds --no-build to skip build step
    Also refactors much of the code and adds more console statements

--- src/sentry.cr
+++ src/sentry.cr
@@ -7,29 +7,33 @@ run_args = [] of String
 files = ["./src/**/*.cr", "./src/**/*.ecr"]
 files_cleared = false
 show_help = false
+should_build = true
 
 OptionParser.parse! do |parser|
   parser.banner = "Usage: ./sentry [options]"
   parser.on(
-    "-r RUN_COMMAND",
-    "--run=RUN_COMMAND",
+    "-b COMMAND",
+    "--build=COMMAND",
+    "Overrides the default build command") { |command| build_command = command }
+  parser.on(
+    "--build-args=ARGS",
+    "Specifies arguments for the build command") do |args|
+    args_arr = args.strip.split(" ")
+    build_args = args_arr if args_arr.size > 0
+  end
+  parser.on(
+    "--no-build",
+    "Skips the build step") { should_build = false }
+  parser.on(
+    "-r COMMAND",
+    "--run=COMMAND",
     "Overrides the default run command") { |command| run_command = command }
   parser.on(
-    "--run-args=RUN_ARGS",
+    "--run-args=ARGS",
     "Specifies arguments for the run command") do |args|
-      args_arr = args.strip.split(" ")
-      run_args = args_arr if args_arr.size > 0
-    end
-  parser.on(
-    "--build-args=BUILD_ARGS",
-    "Specifies arguments for the build command") do |args|
-      args_arr = args.strip.split(" ")
-      build_args = args_arr if args_arr.size > 0
-    end
-  parser.on(
-    "-b BUILD_COMMAND",
-    "--build=BUILD_COMMAND",
-    "Overrides the default build command") { |command| build_command = command }
+    args_arr = args.strip.split(" ")
+    run_args = args_arr if args_arr.size > 0
+  end
   parser.on(
     "-w FILE",
     "--watch=FILE",
@@ -43,8 +47,7 @@ OptionParser.parse! do |parser|
   parser.on(
     "-i",
     "--info",
-    "Shows the values for build command, run command, and watched files"
-    ) do
+    "Shows the values for build/run commands, build/run args, and watched files") do
     puts "
       build:      #{build_command}
       build args: #{build_args}
@@ -66,20 +69,24 @@ module Sentry
   FILE_TIMESTAMPS = {} of String => String # {file => timestamp}
 
   class ProcessRunner
-
     getter app_process : (Nil | Process) = nil
+    property should_build : Bool = true
+    property files = [] of String
 
-    def initialize(build_command : String, build_args : Array(String), run_command : String, run_args : Array(String), files : Array(String))
+    def initialize(
+                   build_command : String,
+                   build_args : Array(String),
+                   run_command : String,
+                   run_args : Array(String))
       @app_built = false
       @build_command = build_command
       @build_args = build_args
       @run_command = run_command
       @run_args = run_args
-      @files = [] of String
-      @files = files
     end
 
     private def build_app_process
+      puts "🤖  compiling [app_name]..."
       build_args = @build_args
       if build_args.size > 0
         Process.run(@build_command, build_args, shell: true, output: true, error: true)
@@ -89,6 +96,15 @@ module Sentry
     end
 
     private def create_app_process
+      app_process = @app_process
+      if app_process.is_a? Process
+        unless app_process.terminated?
+          puts "🤖  killing [app_name]..."
+          app_process.kill
+        end
+      end
+
+      puts "🤖  starting [app_name]..."
       run_args = @run_args
       if run_args.size > 0
         @app_process = Process.new(@run_command, run_args, output: true, error: true)
@@ -101,13 +117,10 @@ module Sentry
       File.stat(file).mtime.to_s("%Y%m%d%H%M%S")
     end
 
+    # Compiles and starts the application
+    #
     def start_app
-      app_process = @app_process
-      if app_process.is_a? Process
-        app_process.kill unless app_process.terminated?
-      end
-
-      puts "🤖  compiling [app_name]..."
+      return create_app_process unless @should_build
       build_result = build_app_process()
       if build_result && build_result.success?
         @app_built = true
@@ -118,6 +131,8 @@ module Sentry
       end
     end
 
+    # Scans all of the `@files`
+    #
     def scan_files
       file_changed = false
       app_process = @app_process
@@ -144,9 +159,10 @@ process_runner = Sentry::ProcessRunner.new(
   build_command: build_command,
   build_args: build_args,
   run_command: run_command,
-  run_args: run_args,
-  files: files
+  run_args: run_args
 )
+process_runner.should_build = should_build
+process_runner.files = files
 
 puts "🤖  Your SentryBot is vigilant. beep-boop..."
 
